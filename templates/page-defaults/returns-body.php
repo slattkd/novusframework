533e2e229c7876c5c5e0397585c1f0ab0a415f1b
dane slatten <dane@few.io>
2022-11-11 11:18:43 -0600

    [dane] content for legal modal links

---
+++ templates/page-defaults/returns-body.php
@@ -0,0 +1,40 @@
+<?php
+$co['name'] = 'Supernatural Man, LLC';
+$co['email'] = 'support@becomesupernatural.com';
+$co['phone'] = '1-800-251-9316';
+$co['address1'] = '300 Main St';
+$co['address2'] = '(Suite 1303)';
+$co['cityzip'] = 'Madison, NJ  07940';
+$co['city'] = 'New York';
+$co['state'] = 'NY';	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,500,600,700,800" rel="stylesheet">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<link href="./shared/css/base.css" rel="stylesheet">
+	<link href="./shared/css/legal-copy.css" rel="stylesheet">
+</head>
+		
+<div class="content px-2">	
+
+<h1 class="">Return Policy</h1>
+
+<h2>Supplement Products</h2>
+<p>All of our products are supplements with shipping.<br>
+We issue refunds for supplement products within 90 days of the original purchase of the product.<br>
+The credit card or form of payment will be directly refunded once requested.<br>
+We recommend contacting us for assistance if you experience any issues.<br>
+</p>
+
+<h2>Contact Us</h2>
+<p>If you have any questions about our Return Policy, please contact us:
+– By visiting this page on our website: <a href=support.php target="_blank">Support</a>
+</p>
+
+
+</div>
+	
+</html>
