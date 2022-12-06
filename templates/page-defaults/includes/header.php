8e7006c2f446b34e89680193e8fdd6f99f356765
Mike McBrien <mike@pineapple.co>
2022-12-05 22:28:54 -0500

    Header cleaning
    Signed-off-by: Mike McBrien <mike@pineapple.co>

--- templates/page-defaults/includes/header.php
+++ templates/page-defaults/includes/header.php
@@ -26,63 +26,46 @@
 
 
 <meta charset="UTF-8">
-<meta http-equiv="content-type" content="text/html; charset=UTF-8"> <!-- double check if needed -->
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-<meta http-equiv="X-UA-Compatible" content="IE=Edge"><!-- double check if needed -->
-<meta name="format-detection" content="telephone=no"><!-- double check if needed -->
-
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="format-detection" content="telephone=no">
 <meta name="google-site-verification" content="<?php echo $site['googleSiteVerification'];?>" />
 
-<!-- CSS -->
-<!-- <script src="https://cdn.tailwindcss.com"></script>  <!-- Should not be needed for production -->
 <link rel="stylesheet" href="/css/main.css?v=3">
 <link rel="stylesheet" href="/css/legal-copy.css?v=2">
 <link rel="stylesheet" href="/css/base.css?v=3">
 
 
 <?php if ($site['debug'] !== true) { //Don't need to run this on local?>
-<!-- Google Tag Manager -->
+
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','<?php echo $site['GTMContainer'];?>');</script>
-<!-- End Google Tag Manager -->
 
-<!-- Google Tag Manager (noscript) -->
 <noscript>
     <iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $site['GTMContainer'];?>"
         height="0" width="0" style="display:none;visibility:hidden">
     </iframe>
 </noscript>
-<!-- End Google Tag Manager (noscript) -->
 <?php } ?>
 
-<!-- Javascript -->
 <script src="//unpkg.com/alpinejs@3.10.5/dist/cdn.min.js" defer></script>
 <script src="//<?php echo $_SERVER['HTTP_HOST'];?>/js/pristine.min.js" defer></script>
 <script src="//<?php echo $_SERVER['HTTP_HOST'];?>/js/basic-modal.js" defer></script>
 <script src="//<?php echo $_SERVER['HTTP_HOST'];?>/js/countdown.js"></script>
 
-
-
-<!-- Next 4 from order.php -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous" media="print" onload="this.media='all'">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,500,600,700,800" rel="stylesheet" media="print" onload="this.media='all'">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet" media="print" onload="this.media='all'">
 <link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700|Cardo:400,400italic,700,800' rel='stylesheet' type='text/css'  media="print" onload="this.media='all'"/>
 
-<!--<link rel="stylesheet" href="/css/jquery.fancybox.css">-->
-<!--<link href="//<?php echo $_SERVER['HTTP_HOST'];?>/css/buy-safe.css" rel="stylesheet"> -->
-<!-- <link href="//<?php echo $_SERVER['HTTP_HOST'];?>/css/main.css" rel="stylesheet" media="print" onload="this.media='all'"> -->
-<!-- <link href="//<?php echo $_SERVER['HTTP_HOST'];?>/css/upsell.css" rel="stylesheet" media="print" onload="this.media='all'" > -->
-
 <?php
+//temporary log rocket tracking
 if ($_SERVER['HTTP_HOST'] == $site['logRocketDomain']) {
     ?>
-    <!-- Temp log rocket logging -->
     <script src="https://cdn.lr-in-prod.com/LogRocket.min.js" crossorigin="anonymous" defer></script>
     <script>window.LogRocket && window.LogRocket.init('<?php echo $site['logRocketTracking'] ?>');</script>
 <?php } ?>
